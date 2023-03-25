8e4ba8ed8a18594ece2b4269e64eaa5605ed2da8
Aaron Canales <aaroncanales0107@gmail.com>
2023-03-25 12:11:08 -0700

    backgound and ground done

--- assets/game.js
+++ assets/game.js
@@ -1,7 +1,55 @@
 var config = {
-    width: 1200,
-    height: 700,
-    scene: [Scene1]
+    type: Phaser.AUTO,
+    width: 1282,
+    height: 750,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+    
 }
 
 var game = new Phaser.Game(config)
+
+
+function preload ()
+{
+    this.load.image("background", "/assets/images/GAME TILESET/Background_01.png")
+    this.load.image("ground", "/assets/images/GAME TILESET/Platformer/Ground_04.png")
+    this.load.image("ground1", "/assets/images/GAME TILESET/Platformer/Ground_02.png")
+    this.load.image("ground2", "/assets/images/GAME TILESET/Platformer/Ground_10.png")
+    this.load.image("ground3", "/assets/images/GAME TILESET/Platformer/Ground_11.png")
+    this.load.image("ground4", "/assets/images/GAME TILESET/Platformer/Ground_12.png")
+    this.load.image("ground5", "/assets/images/GAME TILESET/Platformer/Ground_08.png")
+}
+
+var platforms
+function create ()
+{
+    this.background = this.add.image(0,0,"background")
+    this.background.setOrigin(0,0)
+
+    platforms = this.physics.add.staticGroup();
+    platforms.create(65, 685, 'ground').setScale(1).refreshBody();   
+    platforms.create(193, 685, 'ground1').setScale(1).refreshBody(); 
+    platforms.create(321, 685, 'ground2').setScale(1).refreshBody(); 
+    platforms.create(449, 685, 'ground3').setScale(1).refreshBody(); 
+    platforms.create(577, 685, 'ground3').setScale(1).refreshBody(); 
+    platforms.create(705, 685, 'ground3').setScale(1).refreshBody(); 
+    platforms.create(833, 685, 'ground3').setScale(1).refreshBody(); 
+    platforms.create(961, 685, 'ground4').setScale(1).refreshBody();
+    platforms.create(1089, 685, 'ground1').setScale(1).refreshBody(); 
+    platforms.create(1217, 685, 'ground5').setScale(1).refreshBody(); 
+}
+
+function update ()
+{
+}
--- assets/scene1.js
+++ assets/scene1.js
@@ -1,15 +1,27 @@
-class Scene1 extends Phaser.Scene{
+/*class Scene1 extends Phaser.Scene{
     constructor(){
         super("bootGame")
     }
-
     preload(){
         this.load.image("background", "/assets/images/GAME TILESET/Background_01.png")
+        this.load.image("ground", "/assets/images/GAME TILESET/Platformer/Ground_04.png")
     }
-
+    
     create(){
         this.background = this.add.image(0,0,"background")
         this.background.setOrigin(0,0)
+
+        var platforms
+        platforms = this.physics.add.staticgroup()
+       
+        platforms.create(400,500, 'ground').setScale(2).refreshBody()
+
+        /*this.ground = this.add.image(0,622,"ground")
+        this.ground.setOrigin(0,0)
     }
 
-}
+    update(){
+        game.physics.arcade.collide(platforms)
+    }
+    
+}*/
